fa127ae39b47fb42454c8063ba13a20450fa0b89
biancapcr <biancacristina.pocora@gmail.com>
2026-01-03 17:31:02 +0100

    [feat]: added addtask and tasklist pages

---
+++ src/pages/AddTask.jsx
@@ -0,0 +1,11 @@
+export default function AddTask() {
+  return (
+    <section>
+      {/* titolo della pagina */}
+      <h1 style={{ margin: 0 }}>aggiungi task</h1>
+
+      {/* contenuto placeholder in attesa del form */}
+      <p style={{ marginTop: "8px" }}>form</p>
+    </section>
+  );
+}
---
+++ src/pages/TaskList.jsx
@@ -0,0 +1,11 @@
+export default function TaskList() {
+  return (
+    <section>
+      {/* titolo della pagina */}
+      <h1 style={{ margin: 0 }}>lista dei task</h1>
+
+      {/* contenuto placeholder in attesa della tabella */}
+      <p style={{ marginTop: "8px" }}>tabella task</p>
+    </section>
+  );
+}

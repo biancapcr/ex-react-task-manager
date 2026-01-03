04ddaa831876ee6e4ea895f08172abf6399ef968
biancapcr <biancacristina.pocora@gmail.com>
2026-01-03 17:54:55 +0100

    [fix]: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import TaskList from "./pages/TaskList.jsx";
 import AddTask from "./pages/AddTask.jsx";
+import { GlobalProvider } from "./context/GlobalContext.jsx";
 
 export default function App() {
   return (
-    /* browserrouter abilita il routing lato client */
-    <BrowserRouter>
-      {/* la navbar resta visibile su tutte le rotte */}
-      <Navbar />
+    <GlobalProvider>
+      {/* browserrouter abilita il routing lato client */}
+      <BrowserRouter>
+        {/* la barra di navigazione è visibile su tutte le pagine */}
+        <Navbar />
 
-      {/* contenitore principale delle pagine */}
-      <main style={{ padding: "16px" }}>
-        <Routes>
-          {/* rotta principale: visualizza la lista dei task */}
-          <Route path="/" element={<TaskList />} />
+        {/* contenitore principale delle pagine */}
+        <main style={{ padding: "16px" }}>
+          <Routes>
+            {/* rotta principale: visualizza la lista dei task */}
+            <Route path="/" element={<TaskList />} />
 
-          {/* rotta dedicata alla creazione di un nuovo task */}
-          <Route path="/add" element={<AddTask />} />
+            {/* rotta dedicata all'aggiunta di un nuovo task */}
+            <Route path="/add" element={<AddTask />} />
 
-          {/* rotta di fallback per url non validi */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+            {/* rotta di fallback per percorsi non validi */}
+            {/* l'utente viene reindirizzato alla home */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
